247b7d2857831c033e20fa4f4976c89c8ea7741d
Phoebe <phyuhala@gmail.com>
2024-03-10 13:47:03 -0700

    Create home hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h2>page</h2>
+      <Hero/>
     </div>
   );
 }
