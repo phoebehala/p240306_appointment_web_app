651e3af52425a05e3cff5d5bdd002f4de4860631
Phoebe <phyuhala@gmail.com>
2024-03-30 14:34:18 -0700

    Next.js route

---
+++ app/(route)/search/[cname]/page.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+
+
+function page({children}) {
+  return (
+      <div>{ children }</div>
+  )
+}
+
+export default page
---
+++ app/(route)/search/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function layout({children}) {
+    return (
+        <div>{ children }</div>
+    )
+  }
+  
+export default layout
